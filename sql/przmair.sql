ecd05a4cce4cf3bdab062110b02bf18c5ebe1bf4
Ryan Pace Sloan <ryansdeal@hotmail.com>
2014-11-05 14:51:17 -0700

    I realized that in order to change a flight we will need to make and keep track of itineraries, so I made a table to store serialized objects (objects as long strings)

--- sql/przmair.sql
+++ sql/przmair.sql
@@ -32,19 +32,35 @@ CREATE TABLE profile (
 
 CREATE TABLE flight (
 	flightId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	templateId INT UNSIGNED NOT NULL,
 	flightNumber VARCHAR(15),
 	originAirport VARCHAR(10) NOT NULL,
 	destinationAirport VARCHAR(10) NOT NULL,
 	departureTime DATETIME NOT NULL,
-	/*arrivalTime DATETIME NOT NULL,*/
+	arrivalTime DATETIME NOT NULL,
 	duration TIME NOT NULL,
-	totalAvailableTickets INT UNSIGNED NOT NULL,
+	totalSeatsOnPlane INT UNSIGNED NOT NULL,
 	totalTicketsSold INT UNSIGNED NOT NULL,
-	totalNumAvailableSeats INT UNSIGNED NOT NULL,
+	totalSeatsAvailable INT UNSIGNED NOT NULL,
 	totalNumConfirmedSeats INT UNSIGNED NOT NULL,
+	INDEX(originAirport),
+	INDEX(destinationAirport),
+	INDEX(departureTime),
+	INDEX(arrivalTime),
+	INDEX(duration),
+	UNIQUE(flightNumber),
 	PRIMARY KEY (flightId),
-	UNIQUE(flightNumber)
+	FOREIGN KEY (templateId) REFERENCES template(templateId)
+);
 
+create TABLE template (
+	templateId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	flightNumber VARCHAR(15) NOT NULL,
+	originAirport VARCHAR(10) NOT NULL,
+	destinationAirport VARCHAR(10) NOT NULL,
+	departureTime DATETIME NOT NULL,
+	arrivalTime DATETIME NOT NULL,
+	duration TIME NOT NULL
 );
 
 CREATE TABLE ticket (
@@ -75,15 +91,16 @@ CREATE TABLE traveler (
 	travelerLastName VARCHAR(75) NOT NULL,
 	travelerDateOfBirth DATE NOT NULL,
 	profileId INT UNSIGNED NOT NULL,
+	INDEX(profileId),
 	PRIMARY KEY (travelerId),
-	FOREIGN KEY (profileId) REFERENCES profile(profileId),
-	INDEX(profileId)
+	FOREIGN KEY (profileId) REFERENCES profile(profileId)
+
 );
 
 CREATE TABLE transaction (
 	transactionId INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	profileId INT UNSIGNED NOT NULL,
-	amount DOUBLE UNSIGNED,
+	amount DECIMAL(5,2) UNSIGNED,
 	dateApproved DATETIME,
 	cardToken VARCHAR(124),
 	stripeToken VARCHAR(124),
@@ -102,13 +119,3 @@ CREATE TABLE airport (
 	PRIMARY KEY (airportId)
 );
 
-CREATE TABLE itinerary (
-	itineraryId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	profileId INT UNSIGNED NOT NULL,
-	flightObjectDepart VARCHAR(500) NOT NULL, /*SERIALIZED STRING*/
-	flightObjectReturn VARCHAR(500) NOT NULL, /*SERIALIZED STRING*/
-	INDEX(profileId),
-	PRIMARY KEY(itineraryId),
-	FOREIGN KEY (profileId) REFERENCES profile(profileId)
-
-);
