eed0d9edb0fe48802f63610790f86a71442a94f6
Ryan Pace Sloan <ryansdeal@hotmail.com>
2014-11-10 14:07:48 -0700

    finished script for weekday and weekend

--- php/seed_data/week_day_schedule_builder.php
+++ php/seed_data/week_day_schedule_builder.php
@@ -1,7 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zwg2
- * Date: 11/10/14
- * Time: 1:39 PM
- */ 
+function readWeekdayCSV(&$mysqli,$fileName)
+{
+	if(($filePointer = fopen($fileName, "r")) === false){
+		throw(new RuntimeException("Unable to Open $fileName"));
+	}
+
+	$query = "INSERT INTO weekdaySchedule (origin, destination, duration, departure, arrival, flightNum, price) VALUES(?, ?, ?, ?, ?,
+				?,?)";
+	$statement = $mysqli->prepare($query);
+	if($statement === false) {
+		throw(new mysqli_sql_exception("Unable to prepare statement"));
+	}
+
+	$row = 1;
+	while(($output = fgetcsv($filePointer, 0, ",")) !== false) {
+
+		$num = count($output);
+		$wasClean = $statement->bind_param("ssssssd", $output[0], $output[1], $output[2], $output[3],
+			$output[4], $output[5], $output[6]);
+		if($wasClean === false) {
+			throw(new mysqli_sql_exception("Unable to bind parameters"));
+		}
+		if($statement->execute() === false) {
+			throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+		}
+
+		if(empty($output[7]) === false) {
+			$wasClean = $statement->bind_param("ssssssd", $output[0], $output[1], $output[2], $output[7],
+				$output[8], $output[8], $output[9]);
+			if($wasClean === false) {
+				throw(new mysqli_sql_exception("Unable to bind parameters"));
+			}
+			if($statement->execute() === false) {
+				throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+			}
+
+			if(empty($output[10]) === false) {
+				$wasClean = $statement->bind_param("ssssssd", $output[0], $output[1], $output[2], $output[10],
+					$output[11], $output[12], $output[13]);
+				if($wasClean === false) {
+					throw(new mysqli_sql_exception("Unable to bind parameters"));
+				}
+				if($statement->execute() === false) {
+					throw(new mysqli_sql_exception("Unable to execute mySQL statement"));
+				}
+
+			}
+		}
+		echo "Statement Executed, $num fields in line $row  \n";
+		echo "--------------------------------------------- \n";
+		for ($c=0; $c < $num; $c++) {
+			echo "row: $row position: $c -> ".$output[$c]."\n";
+		}
+		$row++;
+
+	}
+	if(!fclose($filePointer)){
+		throw(new RuntimeException("Unable to close $fileName"));
+	}
+}
+?>
