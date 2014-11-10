72f5e9fcd14e91bea1777f53aac4247999319131
Ryan Pace Sloan <ryansdeal@hotmail.com>
2014-11-10 16:34:47 -0700

    working on executing script to fill schedule table

--- php/main.php
+++ php/main.php
@@ -1,15 +1,16 @@
 <?php
-require_once("seed_data/weekDAY_CSV_export-Table_1.csv");
-require_once("seed_data/week_day_schedule_builder");
+	require_once("week_day_schedule_builder.php");
+	require_once("week_end_schedule_builder.php");
 	mysqli_report(MYSQLI_REPORT_STRICT);
-	//localhost, userName, password, DBname
+
 	echo "TEST BEGIN";
 	try {
+		//localhost, userName, password, DBname
 		$mysqli = new mysqli("localhost", "store_ryan", "trillpontlureactscala", "przm");
-		readWeekdayCSV($mysqli, weekDAY_CSV_export - Table_1 . csv);
-		readWeekendCSV($mysqli, weekEND_CSV_export - Table_1 . csv);
+		readWeekdayCSV($mysqli, "weekDAY_CSV_export-Table_1.csv");
+		readWeekendCSV($mysqli, "weekEND_CSV_export-Table_1.csv");
 	} catch (Exception $exception){
-			$exception->getMessage();
+			echo $exception->getMessage();
 		}
 	echo "TEST END";
 ?>
