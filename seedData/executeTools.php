d140675677a0d7cebc07a7af2e07a4a01377b6de
Ryan Pace Sloan <ryansdeal@hotmail.com>
2014-11-19 13:25:58 -0700

    re committing as team is missing some files https://app.asana.com/0/19248945971931/19594215856468

---
+++ seedData/executeTools.php
@@ -0,0 +1,21 @@
+<?php
+require_once("/etc/apache2/capstone-mysql/przm.php");
+$mysqli = MysqliConfiguration::getMysqli();
+
+require_once("tools.php");
+$baseDate = "2014-12-01";
+$fileName = "weekDayCsv.csv";
+$totalSeats = 25;
+$numDays = 5;
+readCSV($mysqli, $fileName,$baseDate, $totalSeats, $numDays);
+echo"<p> weekDay seed data set to flight </p><br>";
+echo"***********************************************************************************************";
+
+$baseDate = "2014-12-06";
+$fileName = "weekEndCsv.csv";
+$totalSeats = 25;
+$numDays = 2;
+readCSV($mysqli, $fileName, $baseDate, $totalSeats, $numDays);
+echo"<p> weekEnd seed data set to flight </p><br>";
+
+?>
